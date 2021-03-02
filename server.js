d43096d7cc10c2358517bd603bda58671ff33d8b
Gamerman2001 <Flash@heysparkbox.com>
2021-03-02 14:49:14 -0500

    Feat: adding static css to express server style: added styles.css to project
    feat: serving static css file
    - we use the "express.static()" funciton to direct our app on where to locate the css file
    - file imported will be in src/css/styles.css
    - adding any style to the css file was reflected in the html
    - checking the sources in browser shows that the css file has been loaded in
    
    import stylesheet into html
    
    style: linting fixes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import path from 'path';
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'src/css')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/html/index.html'));
 });
