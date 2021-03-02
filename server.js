b6ecc33a3bda7fbb89c699073ead3d615dd89dc5
Gamerman2001 <Flash@heysparkbox.com>
2021-03-02 14:56:48 -0500

    feat: serving static css file - we use the "express.static()" funciton to direct our app on where to locate the css file - file imported will be in src/css/styles.css - adding any style to the css file was reflected in the html - checking the sources in browser shows that the css file has been loaded in

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ import path from 'path';
 const app = express();
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'src/css')))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/html/index.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
