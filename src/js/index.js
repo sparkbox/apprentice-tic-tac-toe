7c31d6f40e2128b5b4bff653024ac5c5a22c2383
Gamerman2001 <Flash@heysparkbox.com>
2021-03-08 14:10:31 -0500

    style: clean up console logs

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,8 @@
 console.debug('Welcome to Tic Tac Toe!');
 function handleClickCell(cell) { // 68
-  console.log('clicked');
   // eslint-disable-next-line no-param-reassign
   cell.innerText = 'X'; // 68
 }
 // eslint-disable-next-line no-undef
 const allGameCells = document.querySelectorAll('.grid-cell');
 allGameCells.forEach((cell) => cell.addEventListener('click', () => { handleClickCell(cell); }));
-console.log(allGameCells);
