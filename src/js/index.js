d025486cda4aae029d71b7c462b8bd9c6c09e20f
Gamerman2001 <Flash@heysparkbox.com>
2021-03-08 14:02:48 -0500

    Feature: Add Cell Click function w/ Tests refactor: removed grid placeholders
    feat: adding click Handling abilities
    - we create a function called "handleClickCell" , this will be called once our game cell is clicked on. It has a parameter called CELL that will  be the node we will be updating. the cell new value will be "X" if clicked on
    - starting from line 8 we will be gathering all game cells using our query selector, each of these cells will be given a clickhandler that will call on the "handleClickCell" function
    
    style: clean up console logs
    
    test: added skeleton test file for index.js
    
    Chore: Install JSDOM
    
    FSA2021-68
    
    Test: Add example test with JSDom
    
    Imported JSDom into the test file and added a simple test for the content of
    a 'Hello, world' paragraph
    
    FSA2021-68
    
    Style: Add space after the console debug message
    
    FSA2021-68
    
    Test: Prepare for DOM testing with index.html
    
    Exported the handleClickCell() function and imported it to the testing file.
    Imported the file system API and used it to store the index.html file as a string,
    which I then passed into a new JSDOM object for use in future tests.
    
    FSA2021-68
    
    Test: Add test for empty grid cell
    
    FSA2021-68
    
    Test: Add test for marking cell with X
    
    FSA2021-68
    
    Test: Add test for marking all grid cells with Xs
    
    FSA2021-68
    
    Chore: Changes to package lock
    
    Test: Replace HTML import
    
    As the HTML for our grid is likely to evolve, directly importing the HTML file means that
    the HTML our tests are based off of will continuously change. This could cause tests that
    pass with consideration to our current code, to fail in the future. To prevent this from
    happening, I removed the HTML import that used the file system and replaced it with a
    gridGame variable written directly into the describe block for handling the click cell
    function.
    
    FSA2021-68
    
    Test: Replace placeholder test with todo block
    
    FSA2021-68
    
    Test: Remove placeholder test
    
    FSA2021-68
    
    Test: Remove placeholder jsdom test
    
    FSA2021-68
    
    Test: Move the click event listener for allGameCells
    
    Moved the event listener for allGameCells to its parent function (i.e. the describe block)
    for use by all tests in the handle click cell function describe block.
    
    FSA2021-68
    
    Test: Add test for making one cell with an X by clicking
    
    Separated the test for marking one grid cell with an X into two tests:
    the original test (i.e. calling handleClickCell directly for one cell)
    the new test (calling click on a gridCell)
    
    FSA2021-68
    
    test: test description updated
    
    style: remove lingering comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 console.debug('Welcome to Tic Tac Toe!');
+
+function handleClickCell(cell) {
+  // eslint-disable-next-line no-param-reassign
+  cell.innerText = 'X';
+}
+// eslint-disable-next-line no-undef
+const allGameCells = document.querySelectorAll('.grid-cell');
+allGameCells.forEach((cell) => cell.addEventListener('click', () => { handleClickCell(cell); }));
+
+export default handleClickCell;
---
+++ src/js/index.spec.js
@@ -0,0 +1,38 @@
+import jsdom from 'jsdom';
+import handleClickCell from './index';
+
+const { JSDOM } = jsdom;
+
+describe('Handle click cell function', () => {
+  const gridGame = new JSDOM(`<div class="game-grid">
+    <div class="grid-cell util-no-top-border util-no-left-border"></div>
+    <div class="grid-cell util-no-top-border"></div>
+    <div class="grid-cell util-no-top-border util-no-right-border"></div>
+    <div class="grid-cell util-no-left-border"></div>
+    <div class="grid-cell"></div>
+    <div class="grid-cell util-no-right-border"></div>
+    <div class="grid-cell util-no-bottom-border util-no-left-border"></div>
+    <div class="grid-cell util-no-bottom-border"></div>
+    <div class="grid-cell util-no-bottom-border util-no-right-border"></div>
+  </div>
+`);
+  // As there are multiple grid cell elements, this will only select the first one.
+  const gridCell = gridGame.window.document.querySelector('.grid-cell');
+  const allGameCells = gridGame.window.document.querySelectorAll('.grid-cell');
+  allGameCells.forEach((cell) => cell.addEventListener('click', () => { handleClickCell(cell); }));
+  it('Shows an empty grid cell before any choices have been made', () => {
+    expect(gridCell.textContent).toBe('');
+  });
+  it('Marks a grid cell with an "X" using the handleClickCell function directly', () => {
+    handleClickCell(gridCell);
+    expect(gridCell.innerText).toBe('X');
+  });
+  it('Marks a grid cell with an "X" by clicking', () => {
+    gridCell.click();
+    expect(gridCell.innerText).toBe('X');
+  });
+  it('Marks all grid cells with an "X"', () => {
+    allGameCells.forEach((cell) => cell.click());
+    allGameCells.forEach((cell) => expect(cell.innerText).toBe('X'));
+  });
+});
