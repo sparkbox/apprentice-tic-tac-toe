9c4bf5e925b4b23896dea3192ba11150520277ae
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-08 15:02:40 -0500

    Test: Add example test with JSDom
    Imported JSDom into the test file and added a simple test for the content of
    a 'Hello, world' paragraph
    
    FSA2021-68

--- src/js/index.spec.js
+++ src/js/index.spec.js
@@ -1,6 +1,18 @@
+import jsdom from 'jsdom';
+
+const { JSDOM } = jsdom;
+
 describe('index.js tests will go here', () => {
   it('expects first test to equal 1', () => {
     const firstTest = 1;
     expect(firstTest).toBe(1);
+  });
+});
+
+describe('replacing text in the paragraph tag', () => {
+  const dom = new JSDOM('<doctype html><p>Hello, world</p>');
+  const domParagraph = dom.window.document.querySelector('p');
+  it('expects the paragraph to have a value of "Hello, world"', () => {
+    expect(domParagraph.textContent).toBe('Hello, world');
   });
 });
