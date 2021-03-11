032446a2f86fcc8334dbf809c774e9f20cecf771
Gamerman2001 <Flash@heysparkbox.com>
2021-03-11 18:42:16 -0500

    refactor: added absolute src folder path - instead of importing the .js files and .css files one by one, I allowed us to serve our app any javascript or css file in our src directory

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import path from 'path';
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'src/')));
+app.use(express.static(path.join(__dirname, 'src')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/html/index.html'));
 });
