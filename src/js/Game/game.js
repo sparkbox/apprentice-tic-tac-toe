a814efd67265c50b5c992aac63bc983442f0b744
Sheridan McKisick <sheridan@heysparkbox.com>
2021-03-12 16:45:13 -0500

    Add notes to winning positions for easier vizualization

--- src/js/Game/game.js
+++ src/js/Game/game.js
@@ -1,14 +1,14 @@
 export const gameBoard = new Array(9).fill(null);
 
 const winningPositions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
+  [0, 1, 2], // Horizontal top
+  [3, 4, 5], // Horizontal middle
+  [6, 7, 8], // Horizontal bottom
+  [0, 3, 6], // Verical left
+  [1, 4, 7], // Vertical middle
+  [2, 5, 8], // Vertical right
+  [0, 4, 8], // Diagonal (L to R)
+  [2, 4, 6], // Diagonal (R to L)
 ];
 
 class Game {
