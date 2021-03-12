c27fab3b96a16b1cebf72cfe429093574d110984
Gamerman2001 <Flash@heysparkbox.com>
2021-03-11 19:12:56 -0500

    refactor: game.js logic..... lets talk ok.... so we wanted to talk about game logic. Here we are blending a little splash of FP and some OOP. - first lets create a gameState ...... this is going to be where all the various states of our game can be located. For the constraints of what we need currently it is just which player is going between X or O - next we got this Game class created. We will have some OO logic to create method calls which will alter our game state once instantiated. The Game class will be instantiated with a state that is passed in via our constructor. The only method we have in here is our nextPlayer method which will conveniently change our current player from X to O - on line 16 when this game file is ran, there will be an instance of a game being created. We will pass this game const our initial gamestate for it to alter - finally on line 18 is where all the magic will be exported. Our current method that will hand information to the UI is this handleClickCell function extrated from the index.js .... this will currently be our default export.... it may change down the line as we alter the game.

---
+++ src/js/Game/game.js
@@ -0,0 +1,22 @@
+console.log('in game')
+const gameState = {
+  currentPlayer: 'X',
+};
+
+class Game {
+  constructor(state) {
+    this.state = state;
+  }
+
+  nextPlayer() {
+    this.state = { currentPlayer: this.state.currentPlayer === 'X' ? 'O' : 'X' }
+  }
+}
+
+const game = new Game(gameState);
+
+export default function handleClickCell(cell) { // 68
+  // eslint-disable-next-line no-param-reassign
+  cell.innerText = game.state.currentPlayer;
+  game.nextPlayer();
+}
