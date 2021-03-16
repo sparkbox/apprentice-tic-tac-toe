10e57ec833a07e2132f8dbd34d762f775dce8f25
Rise Erpelding <rise@heysparkbox.com>
2021-03-16 15:45:23 -0400

    Refactor: update existing tests
    - This builds on a previous refactoring to remove repetitive code by
    creating functions for pieces of code that are repeatedly created.
    -Condenses separate JSDOM instances being used in tests into one.
    - Since subheader tests have been written in a different style to
    better utilize beforeEach() (which will minimize data leaking from test
    to test), this also updates the style of the tests to match.
    - With the second block of tests (Toggling between Xs and Os), this was
    not immediately possible without heavier refactoring, so it is left
    largely the same. This block would need more refactoring if tests were
    to be added to it, but is currently just fine as is.
    - As we go forward, it might be helpful to move index.spec.js into a
    separate folder for tests. This way, we can run tests specifically for
    the Game class in a game.spec.js file and the two files can be together
    in one folder. If we have a testing folder, we could potentially have a
    separate file for helpful functions for the tests. Here, just
    highlighting what these helper functions to clarify, especially in the
    event that they are ever separated from index.spec.js.

--- src/js/index.spec.js
+++ src/js/index.spec.js
@@ -4,22 +4,8 @@ import { handleClickCell, Game } from './Game/game';
 
 const { JSDOM } = jsdom;
 
-function ticTacToeUI() {
-  return new JSDOM(`<div class="game-grid">
-<div class="grid-cell util-no-top-border util-no-left-border"></div>
-<div class="grid-cell util-no-top-border"></div>
-<div class="grid-cell util-no-top-border util-no-right-border"></div>
-<div class="grid-cell util-no-left-border"></div>
-<div class="grid-cell"></div>
-<div class="grid-cell util-no-right-border"></div>
-<div class="grid-cell util-no-bottom-border util-no-left-border"></div>
-<div class="grid-cell util-no-bottom-border"></div>
-<div class="grid-cell util-no-bottom-border util-no-right-border"></div>
-</div>
-`);
-}
-
-function createSubheaderAndGrid() {
+// helper function to create the mock tic-tac-toe UI used in the tests
+function createTicTacToeUI() {
   return new JSDOM(`
   <h2 class="subheader"></h2>
   <div class="game-grid">
@@ -36,89 +22,94 @@ function createSubheaderAndGrid() {
 `);
 }
 
+// helper function to add event listeners to cells
+function addHandleClickListener(cells, subheader, game) {
+  cells.forEach((cell) => cell.addEventListener('click', () => { handleClickCell(cell, subheader, game); }));
+}
+
 describe('Handle click cell function', () => {
-  const gridGame = ticTacToeUI();
-  // As there are multiple grid cell elements, this will only select the first one.
-  const gridCell = gridGame.window.document.querySelector('.grid-cell');
-  const allGameCells = gridGame.window.document.querySelectorAll('.grid-cell');
+  let gridGame;
+  let game;
+  let singleGridCell;
+  let allGridCells;
+  let subheader; // not ever defined or used, but needed to pass into handleClickCell
   beforeEach(() => {
-    allGameCells.forEach((cell) => cell.addEventListener('click', () => { handleClickCell(cell); }));
-  });
-  afterEach(() => {
-    allGameCells.forEach((cell) => {
-      // eslint-disable-next-line no-param-reassign
-      cell.innerText = '';
-    });
-    game.currentPlayer = 'X';
+    gridGame = createTicTacToeUI();
+    game = new Game();
+    // As there are multiple grid cell elements, this will only select the first one.
+    singleGridCell = gridGame.window.document.querySelector('.grid-cell');
+    allGridCells = gridGame.window.document.querySelectorAll('.grid-cell');
+    addHandleClickListener(allGridCells, subheader, game);
   });
   it('Shows an empty grid cell before any choices have been made', () => {
-    expect(gridCell.textContent).toBe('');
+    expect(singleGridCell.textContent).toBe('');
   });
   it('Marks a grid cell with an "X" using the handleClickCell function directly', () => {
-    handleClickCell(gridCell);
-    expect(gridCell.innerText).toBe('X');
+    handleClickCell(singleGridCell, subheader, game);
+    expect(singleGridCell.innerText).toBe('X');
   });
   it('Marks a grid cell with an "X" by clicking', () => {
-    gridCell.click();
-    expect(gridCell.innerText).toBe('X');
+    singleGridCell.click();
+    expect(singleGridCell.innerText).toBe('X');
   });
   it('Does not mark a grid that has been marked already', () => {
-    gridCell.click();
-    const click1 = gridCell;
-    gridCell.click();
-    const click2 = gridCell;
-    expect(click1.innerText).toEqual('X');
-    expect(click2.innerText).toEqual('X');
+    singleGridCell.click();
+    const click1GridContent = singleGridCell.innerText;
+    singleGridCell.click();
+    const click2GridContent = singleGridCell.innerText;
+    expect(click1GridContent).toEqual('X');
+    expect(click2GridContent).toEqual('X');
   });
 });
 
 describe('Toggling between Xs and Os', () => {
-  const gridGame = ticTacToeUI();
-  // As there are multiple grid cell elements, this will only select the first one.
-  const allGameCells = gridGame.window.document.querySelectorAll('.grid-cell');
-  allGameCells.forEach((cell) => cell.addEventListener('click', () => { handleClickCell(cell); }));
+  const gridGame = createTicTacToeUI();
+  const game = new Game();
+  const allGridCells = gridGame.window.document.querySelectorAll('.grid-cell');
+  let subheader; // not ever defined or used, but needed to pass into handleClickCell
+  addHandleClickListener(allGridCells, subheader, game);
   // The following for loop will go through and click each cell in the game grid.
   // It will click each cell only once, so a cell will go from blank to either an X or an O,
   // depending on whether the click is even (0, 2, 4, 6, 8) or odd (1, 3, 5, 7).
-  for (let i = 0; i < allGameCells.length; i += 1) {
-    allGameCells[i].click();
+  for (let i = 0; i < allGridCells.length; i += 1) {
+    allGridCells[i].click(); // Simulate clicking each cell in order from index 0 to 8
     // As indexes are zero based, zero will be counted as even.
     if (i % 2 === 0) {
       it(`Shows that a click on a previously unclicked cell will produce an x if said click is an even number (click #${i})`, () => {
-        expect(allGameCells[i].innerText).toBe('X');
+        expect(allGridCells[i].innerText).toBe('X');
       });
     } else {
       it(`Shows that a click on a previously unclicked cell will produce an o if said click is an odd number (click #${i})`, () => {
-        expect(allGameCells[i].innerText).toBe('O');
+        expect(allGridCells[i].innerText).toBe('O');
       });
     }
   }
 });
 
-describe.only('subheader text', () => {
-  let ticTacToeDom;
+describe('subheader text', () => {
+  let gridGame;
   let game;
   let allGridCells;
   let subheader;
   beforeEach(() => {
-    ticTacToeDom = createSubheaderAndGrid();
-    subheader = ticTacToeDom.window.document.querySelector('.subheader');
+    gridGame = createTicTacToeUI();
+    subheader = gridGame.window.document.querySelector('.subheader');
     subheader.innerText = (`It's X's turn`);
     game = new Game();
-    allGridCells = ticTacToeDom.window.document.querySelectorAll('.grid-cell');
+    allGridCells = gridGame.window.document.querySelectorAll('.grid-cell');
     addHandleClickListener(allGridCells, subheader, game);
   });
   it(`shows X's turn in subheader at the beginning of the game`, () => {
     expect(subheader.innerText).toBe(`It's X's turn`);
   });
   it(`shows O's turn in subheader after first turn`, () => {
-    const gridCell = ticTacToeDom.window.document.querySelector('.grid-cell');
+    const gridCell = gridGame.window.document.querySelector('.grid-cell');
     gridCell.click();
     expect(subheader.innerText).toBe(`It's O's turn`);
   });
   it(`show's X's turn in subheader after two turns`, () => {
-    const firstGridCell = ticTacToeDom.window.document.querySelectorAll('.grid-cell')[0];
-    const secondGridCell = ticTacToeDom.window.document.querySelectorAll('.grid-cell')[1];
+    const firstGridCell = gridGame.window.document.querySelectorAll('.grid-cell')[0];
+    const secondGridCell = gridGame.window.document.querySelectorAll('.grid-cell')[1];
     firstGridCell.click();
     secondGridCell.click();
     expect(subheader.innerText).toBe(`It's X's turn`);
