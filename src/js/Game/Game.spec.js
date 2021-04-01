c62c774546012c6851f2e8fa0bf7522aeb4c261d
Sheridan McKisick <sheridan@heysparkbox.com>
2021-04-01 12:25:21 -0400

    Style: Break up long line
    FSA2021-74

--- src/js/Game/Game.spec.js
+++ src/js/Game/Game.spec.js
@@ -3,16 +3,19 @@ import { Game } from './game';
 describe('Use the game board to mark spaces', () => {
   const gameWithBoard = new Game();
   it('Shows a game board before anything has been selected', () => {
-    expect(gameWithBoard.gameBoard).toStrictEqual([null, null, null, null, null, null, null, null, null]);
+    expect(gameWithBoard.gameBoard).toStrictEqual([null, null, null, null,
+      null, null, null, null, null]);
   });
 
   it('Marks the space with current player', () => {
     const markedBoard = gameWithBoard.markBoard(1);
-    expect(gameWithBoard.gameBoard).toStrictEqual([null, 'X', null, null, null, null, null, null, null]);
+    expect(gameWithBoard.gameBoard).toStrictEqual([null, 'X', null, null,
+      null, null, null, null, null]);
   });
   it('Marks the next space with an O', () => {
     gameWithBoard.nextPlayer();
     gameWithBoard.markBoard(0);
-    expect(gameWithBoard.gameBoard).toStrictEqual(['O', 'X', null, null, null, null, null, null, null]);
+    expect(gameWithBoard.gameBoard).toStrictEqual(['O', 'X', null, null,
+      null, null, null, null, null]);
   });
 });
