6edfc32f2dcdfb52c3989b0a3e5a5054db663d50
Gamerman2001 <Flash@heysparkbox.com>
2021-04-01 13:55:58 -0400

    refactor: import Game and HCC - Importing our Game.js file and the code for handleClickCell - we are going to instantiate Game.js after importing it - we use the Game instantiation in our paremeter

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,6 @@
 /* eslint-disable import/extensions */
-import { handleClickCell } from './Game/game.js';
+import Game from './Game/game.js';
 import { updateVersionNumber } from './version.js';
-
-console.debug('Welcome to Tic Tac Toe!');
-
 // eslint-disable-next-line no-undef
 const subheader = document.querySelector('.subheader');
 // eslint-disable-next-line quotes
@@ -13,5 +10,21 @@ const allGameCells = document.querySelectorAll('.grid-cell');
 allGameCells.forEach((cell, index) => cell.addEventListener('click', () => { handleClickCell(cell, subheader, index); }));
 
 const appVersion = document.querySelector('.app-version_version');
+const game = new Game();
 
+console.debug('Welcome to Tic Tac Toe!');
 updateVersionNumber(appVersion);
+
+function handleClickCell(cell, subheading, index, currGame = game) {
+  if (cell.innerText) return;
+  // eslint-disable-next-line no-param-reassign
+  cell.innerText = currGame.currentPlayer;
+  currGame.markBoard(index);
+  currGame.nextPlayer();
+  if (subheading) {
+    // eslint-disable-next-line no-param-reassign
+    subheading.innerText = `It's ${currGame.currentPlayer}'s turn`;
+  }
+}
+
+allGameCells.forEach((cell, index) => cell.addEventListener('click', () => { handleClickCell(cell, subheader, index); }));
