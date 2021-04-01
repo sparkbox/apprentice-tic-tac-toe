99e35498430f1c7fffae9e82a05e216e3ddca7aa
Gamerman2001 <Flash@heysparkbox.com>
2021-04-01 13:56:37 -0400

    refactor: structured file

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,9 @@ import Game from './Game/game.js';
 import { updateVersionNumber } from './version.js';
 // eslint-disable-next-line no-undef
 const subheader = document.querySelector('.subheader');
-// eslint-disable-next-line quotes
-subheader.innerText = (`It's X's turn`);
+subheader.innerText = ('It\'s X\'s turn');
 // eslint-disable-next-line no-undef
 const allGameCells = document.querySelectorAll('.grid-cell');
-allGameCells.forEach((cell, index) => cell.addEventListener('click', () => { handleClickCell(cell, subheader, index); }));
-
 const appVersion = document.querySelector('.app-version_version');
 const game = new Game();
 
